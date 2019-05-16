64a7dedbccf4c1c5301974048f88fd6344644ce7
yaelfr <yaelif100@gmail.com>
2019-05-16 22:00:21 +0300

    pull before update database

--- GymProject/Models/Lesson.cs
+++ GymProject/Models/Lesson.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,8 +11,8 @@ namespace GymProject.Models
         public int LessonId { get; set; }
         public Branch BranchId { get; set; }
         public Training TrainingId { get; set; }
-        public DateTime StartTime { get; set; }
-        public DateTime EndTime { get; set; }
+        public TimeSpan StartTime { get; set; }
+        public TimeSpan EndTime { get; set; }
         public string TeacherName { get; set; }
         public int RegistrantMax { get; set; }
         public int RegistrantNum { get; set; }
