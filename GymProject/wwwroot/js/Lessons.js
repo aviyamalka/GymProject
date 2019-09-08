906b7109643372193e5ac030defdc20ec5ff6436
efratgr10 <efratgr10@gmail.com>
2019-09-08 17:35:51 +0300

    disable search button till all fields full

--- GymProject/wwwroot/js/Lessons.js
+++ GymProject/wwwroot/js/Lessons.js
@@ -2,6 +2,27 @@
     function addZeroInDateTimeFormat() {
 
     }
+
+    debugger;
+    var elements = document.querySelectorAll("#selectedCity, #selectedDate");
+    searchButton = document.querySelector('#searchBtn')
+    searchButton.disabled = true
+    $("#searchBtnLabel").css('background-color', 'pink');
+
+    for (i = 0; i < elements.length; i++) {
+        elements[i].addEventListener('change', () => {
+            let values = []
+            elements.forEach(v => values.push(v.value))
+            if (values.includes('') || values.includes('-עיר-')) {
+                searchButton.disabled = true;
+                $("#searchBtnLabel").css('background-color', 'pink');
+            } else {
+                searchButton.disabled = false;
+                $("#searchBtnLabel").css('background-color', '#d43f3a');
+            }
+        })
+    }
+
     function IsLessonEnabled() {
         $(".regRow").each(function (i, row) {
             var $row = $(row),
